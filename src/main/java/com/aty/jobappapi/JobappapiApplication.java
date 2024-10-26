4e61409c8c18266e1327867619673cc469dfcfd8
Atilla YAS <atilla.yas@gmail.com>
2024-10-26 10:54:42 +0200

    projet springboot créé sans BDD atm + 1ere entité job créé + 1ers tests API OK

---
+++ src/main/java/com/aty/jobappapi/JobappapiApplication.java
@@ -0,0 +1,13 @@
+package com.aty.jobappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobappapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobappapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aty/jobappapi/JobappapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aty.jobappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobappapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
